6d6399db67a9a68ff462e8724e2c81bd568f5804
champion19 <emmalondogo@gmail.com>
2026-03-03 16:16:43 -0500

    style: Add blank line after library directive.

--- lib/core/network/dio_web_impl.dart
+++ lib/core/network/dio_web_impl.dart
@@ -4,6 +4,7 @@
 /// `BrowserHttpClientAdapter(withCredentials: true)` so the browser
 /// includes HttpOnly cookies in every request automatically.
 library;
+
 import 'package:dio/dio.dart';
 import 'package:dio_web_adapter/dio_web_adapter.dart';
 
